d9891b953427c1193965f189a598d20700d09577
z <78839407@qq.com>
2023-12-30 14:08:44 +0400

    z

--- z_buildTRTengine.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8s-pose.pt")  # load a pretrained model (recommended for training)
-success = model.export(format="engine", device=[0, 1])  # export the model to engine format
-assert success
